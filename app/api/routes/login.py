61dcfc256da5b8474cc9df26c71ad6772cad6e97
Sasha Silina <sasha.silina27@gmail.com>
2025-04-09 10:45:01 +0300

    пробы новой структуры

---
+++ app/api/routes/login.py
@@ -0,0 +1,34 @@
+from datetime import timedelta
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app import crud
+from app.api.deps import SessionDep
+from app.core.config import settings
+from app.core import security
+from app.models import Token
+
+router = APIRouter(tags=["login"])
+
+
+@router.post("/login/access-token")
+def login_access_token(
+    session: SessionDep, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+) -> Token:
+    user = crud.authenticate(
+        session=session, email=form_data.username, password=form_data.password
+    )
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+    elif not user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    return Token(
+        access_token=security.create_access_token(
+            user.id, expires_delta=access_token_expires
+        )
+    )
+
+
--- app/main.py
+++ app/main.py
@@ -1,48 +1,7 @@
-####################
-# Файл main.py
+from fastapi import FastAPI
 
-from fastapi import FastAPI, Depends, HTTPException, status
-from app.db import schemas, crud
-from app.api import auth
-from app.db.session import SessionLocal, engine
-from sqlalchemy.orm import Session
-from app.db.session import Base
+from app.api.main import api_router
 
-app = FastAPI(
-    title="Drone Video Service",
-    version="1.0",
-    description="API для сервиса автоматизированной видеосъёмки"
-)
+app = FastAPI()
 
-# Создаём таблицы в БД (для теста)
-Base.metadata.create_all(bind=engine)  # Теперь работает!
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@app.post("/signup", tags=["Регистрация"], summary="Зарегистрироваться", response_model=schemas.User)
-def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
-
-@app.post("/login", tags=["Вход"], summary="Авторизоваться", response_model=schemas.Token)
-def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
-    user = auth.authenticate_user(db, user.email, user.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-        )
-    access_token = auth.create_access_token(data={"sub": user.email})
-    return {"access_token": access_token, "token_type": "bearer"}
-
-@app.get("/users/me")
-def read_current_user(current_user: schemas.User = Depends(auth.get_current_user)):
-    return current_user
+app.include_router(api_router)
